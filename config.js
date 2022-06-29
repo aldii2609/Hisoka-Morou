93b1726fe2b80bbcf78fca01ad8010e337c831c7
aldii2609 <108321145+aldii2609@users.noreply.github.com>
2022-06-29 23:57:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -18,10 +18,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['6281289822290','628999625005']
+global.premium = ['628999625005']
+global.packname = 'Aldi Bot'
+global.author = 'Aldi Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
